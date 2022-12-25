370c1f86f103abfb58a5c59eabcc8113a1d4c7fc
pallavisahu189 <121310964+pallavisahu189@users.noreply.github.com>
2022-12-25 15:26:44 +0000

    Let, const and var

---
+++ .devcontainer/javascript/day_2.js
@@ -0,0 +1,24 @@
+//  Let, const and var ki kahani | Javascript
+
+const accountId = 17102002;
+let accountEmail = 'pallavi@gmail.com';
+var accountPassword = "12345"
+let accountCity = "Raipur"
+
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
+
+// accountId = 111;
+accountEmail = 'pallu@gmail.com';
+accountPassword = "232323"
+accountCity = "Balod"
+
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
+
+// variable ko table ke form me dekhne ke liye
+console.table([accountId, accountEmail, accountPassword, accountCity])
