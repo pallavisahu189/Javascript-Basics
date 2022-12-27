a4212bb4c37051300ee7eb80377c2cc4e77b86ea
pallavisahu189 <121310964+pallavisahu189@users.noreply.github.com>
2022-12-27 17:29:29 +0000

    add two num

---
+++ .devcontainer/javascript/add.js
@@ -0,0 +1,18 @@
+// Add and sub Two Numbers
+
+const num1 = 23;
+const num2 = 32;
+// add two num
+const sum = num1 + num2;
+
+const sub = num2 - num1;
+
+// display the sum
+console.log('sum');
+console.log(sum)
+console.log('The sum of ' + num1 + ' and ' + num2 + ' is: ' + sum);
+
+// display the sub
+console.log('sub')
+console.log(sub)
+console.log('The sub of ' + num1 + ' and ' + num2 + ' is: ' + sub);
---
+++ .devcontainer/javascript/day3.js
@@ -0,0 +1,17 @@
+console.log("hello");
+console.log("my name is pallavi sahu");
+console.log("student of BCA 1st year");
+console.log("compeleted diploma in modern office management");
+console.log("a+b");
+console.log("ab+bc+2bac");
+// alert("hello");         this is not use github
+
+// document.write('hello');
+
+const num1 = 5;
+const num2 = 3;
+// add two num
+const sum = num1 + num2;
+// display the sum
+console.log('The sum of ' + num1 + ' and ' + num2 + ' is: ' + sum);
+
