8bf3784238496edaab263f39ac366b28d84c20e1
pallavisahu189 <121310964+pallavisahu189@users.noreply.github.com>
2022-12-29 17:27:30 +0000

    check even or odd

---
+++ .devcontainer/javascript/day_5.js
@@ -0,0 +1,17 @@
+// program to check if the number is even or odd
+
+
+const number = 20;
+
+//check if the number is even
+
+if(number % 2 == 0) {
+    console.log("The number is even.");
+}
+
+// if the number is odd
+else {
+    console.log("The number is odd.");
+}
+
+
---
+++ .devcontainer/javascript/even.js
@@ -0,0 +1,28 @@
+const num = 21;
+const number = 22;
+
+//check if the number is even
+
+if(num % 2 == 0) 
+{
+    console.log(num + "The number is even.");
+}
+
+else 
+{
+    console.log(num + "The number is odd.");
+}
+
+
+
+if(number % 2 == 1)
+{console.log(number + "the number is odd")}
+
+// if the number is odd
+ 
+else 
+{
+    console.log(number + "The number is even.");
+}
+
+ 
