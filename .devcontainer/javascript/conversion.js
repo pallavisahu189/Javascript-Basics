2a32175c2a6db1bc3faa6711daaf7ee1e452b3e8
pallavisahu189 <121310964+pallavisahu189@users.noreply.github.com>
2022-12-31 07:54:37 +0000

    conversion examples

---
+++ .devcontainer/javascript/conversion.js
@@ -0,0 +1,64 @@
+// this is example 1
+let x = "25"
+console.log(typeof num);
+
+
+// example 2
+let valueInNumber = 25
+console.log(typeof valueInNumber);
+
+
+// example 3
+let a = Number(x)
+console.log(a)
+console.log(typeof a);
+
+
+// example 4
+let n = 15
+let m = 12
+let sum = n + m
+console.log(sum);
+
+
+// example 5
+let o = 14
+let p = "20"
+let add = o + Number(p)
+console.log(add);
+
+
+// example 6
+let z = -4
+let booleanz = Boolean(z)
+console.log(booleanz);
+console.log(typeof z);
+console.log(typeof booleanz);
+
+
+// example 7
+let f = 100
+let booleanf = Boolean(f);
+console.log(booleanf);
+
+
+// example 8
+let r = 0
+let booleanr = Boolean(r);
+console.log(booleanr);
+console.log(typeof booleanr);
+console.log(typeof r);
+
+
+// example 9
+let someNumber = "25+25"
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+// example 10
+let value = "55+25"
+let valueName = Number(value);
+console.log(valueName);
+console.log(typeof valueName);
