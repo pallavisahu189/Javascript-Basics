70a8acfc4dd18114b18ef466c620fc5bdd2e0b66
pallavisahu189 <121310964+pallavisahu189@users.noreply.github.com>
2023-01-02 18:18:00 +0000

    check even and odd number

---
+++ .devcontainer/javascript/day_8.js
@@ -0,0 +1,32 @@
+const v = 15;
+if(v % 2 == 0)
+{console.log (v + "is a even number");}
+else
+{console.log (v + "is a odd number");}
+
+
+
+const u = 16;
+if(u % 2 == 0)
+{console.log (u + "is a even number");}
+else
+{console.log (u + "is a odd number");}
+
+
+
+
+const w = 0;
+if(w % 2 == 0)
+{console.log (w + "is a even number");}
+else
+{console.log (w + "is a odd number");}
+
+
+
+const l = null;
+if(l % 2 == 0)
+{console.log (l + "is a even number");}
+else
+{console.log (l + "is a odd number");}
+
+
