cbec178043299622fbed0781b3653f46d787dde6
pallavisahu189 <121310964+pallavisahu189@users.noreply.github.com>
2023-01-04 17:40:31 +0000

    square and squareroot of number

---
+++ .devcontainer/javascript/day_9.js
@@ -0,0 +1,10 @@
+const number = 25;
+
+const squareroot = Math.sqrt(number);
+console.log(squareroot +" is square root of "+ number);
+
+const square = number**2;
+console.log(square +" is square of "+ number);
+
+
+
