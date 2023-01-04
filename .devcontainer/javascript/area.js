79c881baa3170749b0f4f182ac662a3e6203813e
pallavisahu189 <121310964+pallavisahu189@users.noreply.github.com>
2023-01-04 18:01:59 +0000

    calculate the area of the triangle

---
+++ .devcontainer/javascript/area.js
@@ -0,0 +1,13 @@
+// calculate the area of the triangle
+
+const base = 4;
+const height = 5;
+const area = base*height /2;
+console.log(area);
+console.log(" this is area of triangle " + area + " and its " + base + "and its" + height);
+console.log(`the area of triangle is ${area}`);
+
+
+
+
+
