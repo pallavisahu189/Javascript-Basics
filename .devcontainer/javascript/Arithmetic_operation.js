09e8e83d103e9173d808ed2bd652e180ab584ac8
pallavisahu189 <121310964+pallavisahu189@users.noreply.github.com>
2023-01-05 10:42:06 +0000

    Arithmetic Operation

---
+++ .devcontainer/javascript/Arithmetic_operation.js
@@ -0,0 +1,30 @@
+// Arithmetic Operation 
+
+
+let  a = 2.2;     //addition of number
+let b = 2;
+let add = a+b;
+console.log(add);
+
+let x = 2;      // subtraction of number
+let y = 2;
+let sub = x-y;
+console.log(sub);
+
+
+let  p = 2;     // multiplication of number
+let  q = 2;
+let multi = p*q;
+console.log(multi);
+
+let m = 2;      // division of number
+let n = 2;
+let division = m/n;
+console.log(division);
+
+
+let r = 5;      //// division of number
+let s = 2;
+let D = r/s;
+console.log(D);
+
