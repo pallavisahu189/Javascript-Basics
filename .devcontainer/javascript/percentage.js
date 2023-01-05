dbca58df334c2191090a315755fadc8b93096be6
pallavisahu189 <121310964+pallavisahu189@users.noreply.github.com>
2023-01-05 10:42:44 +0000

    calculation Percentages

---
+++ .devcontainer/javascript/percentage.js
@@ -0,0 +1,9 @@
+// calculation Percentages
+
+const sub1 = 97;
+const sub2 = 97;
+const sub3 = 87;
+const sub4 = 74;
+const sub5 = 66;
+const P = (sub1 + sub2 + sub3 + sub4 + sub5)/5;
+console.log( P + " its parcentage of five subject ");
