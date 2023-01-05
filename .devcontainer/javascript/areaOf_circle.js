6941a26f071e879e4b36e35961d691f13bd6210f
pallavisahu189 <121310964+pallavisahu189@users.noreply.github.com>
2023-01-05 10:41:20 +0000

    Area of Circle

---
+++ .devcontainer/javascript/areaOf_circle.js
@@ -0,0 +1,6 @@
+// this is Area of Circle
+
+const r = 6;
+const pi = 3.14;  
+const result = pi*r*r   // area of circle = pi*R the power 2
+console.log(" this is area of circle: " + result + " its redius: " + r);
